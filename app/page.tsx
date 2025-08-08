1ddf38ec4e429e79795759212f6e2158cdea5f75
muqorrobin22 <achmadmuqorrobinassholih@gmail.com>
2025-08-08 22:43:49 +0700

    Testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+interface DataType {
+  key: React.Key;
+  name: string;
+  age: number;
+  address: string;
+  description: string;
+}
+
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-[100vh]">
