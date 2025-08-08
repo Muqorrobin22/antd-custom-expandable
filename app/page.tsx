856e9ea0c009e3c82320f9778954ed23ab9fd07c
muqorrobin22 <achmadmuqorrobinassholih@gmail.com>
2025-08-08 22:12:45 +0700

    Preparing Project: Add Basic style

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <div className="flex items-center justify-center h-[100vh]">
       {/* Code here */}
       <h1>Code Here</h1>
     </div>
